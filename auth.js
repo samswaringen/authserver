540639414db66118fa49b1372cbdd978340978b6
samswaringen <76227866+samswaringen@users.noreply.github.com>
2021-09-24 10:40:39 -0400

    added swagger api documentation

--- auth.js
+++ auth.js
@@ -5,6 +5,20 @@ var bodyParser = require("body-parser")
 const cors = require('cors');
 const app = express();
 var jsonParser = bodyParser.json()
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express')
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        title: 'Full Stack Auth Server',
+        version: '1.0.0',
+      },
+    },
+    apis: ['authlocal.js'], // files containing annotations as above
+  };
+  
+  const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
 
 let port = process.env.PORT || 9002;
@@ -15,7 +29,7 @@ const refreshSecret = process.env.JWT_REFRESH_KEY;
 
 const refreshTokens = [];
 
-const allowedOrigins = ['http://localhost:3000','https://ipfs.io', 'https://samantics.tez.page', 'https://sam-swaringen-atm-front-end.herokuapp.com/'];
+const allowedOrigins = ['http://localhost:3000','https://ipfs.io', 'https://samantics.tez.page'];
 
 const options = {
   origin: allowedOrigins
@@ -25,6 +39,34 @@ const options = {
 app.use(cors(options));
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
+
+/**
+ * @swagger
+ * /login:
+ *  post:
+ *      summary: Verify username and password as existing customers
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: body
+ *          name: login
+ *          description: The user's username and password
+ *          schema:
+ *              type: object
+ *              required:   
+ *                  - username
+ *                  - password
+ *              properties:
+ *                  username:
+ *                      type: string
+ *                  password:
+ *                      type: string
+ *      responses:
+ *          200:
+ *              description: Will return JWT auth token and refresh token if user found/ return "Username or password does not exist" otherwise
+ */
 
 app.post('/login', jsonParser, async (req, res) => {
     // read username and password from request body
@@ -48,6 +90,29 @@ app.post('/login', jsonParser, async (req, res) => {
         res.send('Username or password incorrect');
     }
 });
+
+/**
+ * @swagger
+ * /loginGoogle:
+ *  post:
+ *      summary: Verify returned email address from Google OAuth
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: body
+ *          name: loginGoogle
+ *          description: The user's email return from Google OAuth
+ *          schema:
+ *              type: object
+ *              required:   
+ *                  - email
+ *              properties:
+ *                  email:
+ *                      type: string
+ *      responses:
+ *          200:
+ *              description: Will return JWT auth token and refresh token if user found/ return "make account" otherwise
+ */
 
 app.post('/loginGoogle', jsonParser, async (req, res) => {
     // read username and password from request body
@@ -75,6 +140,32 @@ app.post('/loginGoogle', jsonParser, async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /loginemp:
+ *  post:
+ *      summary: Verify username and password as existing employees
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: body
+ *          name: loginemp
+ *          description: The employee's username and password
+ *          schema:
+ *              type: object
+ *              required:   
+ *                  - username
+ *                  - password
+ *              properties:
+ *                  username:
+ *                      type: string
+ *                  password:
+ *                      type: string
+ *      responses:
+ *          200:
+ *              description: Will return JWT auth token and refresh token if user found/ return "Username or password does not exist" otherwise
+ */
+
 app.post('/loginemp', jsonParser, async (req, res) => {
     // read username and password from request body
     const { username, password } = req.body;
@@ -96,6 +187,32 @@ app.post('/loginemp', jsonParser, async (req, res) => {
         res.send('Username or password incorrect');
     }
 });
+
+/**
+ * @swagger
+ * /loginatm:
+ *  post:
+ *      summary: Verify username and pin as existing customers
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: body
+ *          name: loginatm
+ *          description: The user's username and pin
+ *          schema:
+ *              type: object
+ *              required:   
+ *                  - username
+ *                  - pin
+ *              properties:
+ *                  username:
+ *                      type: string
+ *                  pin:
+ *                      type: string
+ *      responses:
+ *          200:
+ *              description: Will return JWT auth token and refresh token if user found/ return "Username or password does not exist" otherwise
+ */
 
 app.post('/loginatm', jsonParser, async (req, res) => {
     // read username and password from request body
@@ -119,6 +236,53 @@ app.post('/loginatm', jsonParser, async (req, res) => {
         res.send('Username or pin incorrect');
     }
 });
+
+/**
+ * @swagger
+ * /createuser:
+ *  post:
+ *      summary: Creates new customer object and sends to database, creates temporary JWT with username but no role
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: body
+ *          name: createuser
+ *          description: The user's info for account you are creating
+ *          schema:
+ *              type: object
+ *              required:   
+ *                  - id
+ *                  - dateTime
+ *                  - routing
+ *                  - name
+ *                  - username
+ *                  - email
+ *                  - password
+ *                  - chkAcctNumber
+ *                  - savAcctNumber
+ *              properties:
+ *                  id:
+ *                      type: string
+ *                  dateTime:
+ *                      type: string
+ *                  routing:
+ *                      type: integer
+ *                  name:
+ *                      type: string
+ *                  username:
+ *                      type: string
+ *                  email:
+ *                      type: string
+ *                  password:
+ *                      type: string
+ *                  chkAcctNumber:
+ *                      type: number
+ *                  savAcctNumber:
+ *                      type: number
+ *      responses:
+ *          200:
+ *              description: Will return temp JWT auth token and refresh token when user created
+ */
 
 app.post('/createuser', jsonParser, async (req, res) => {
     // read username and password from request body
@@ -157,6 +321,47 @@ app.post('/createuser', jsonParser, async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /createemp:
+ *  post:
+ *      summary: Creates new employee object and sends to database, creates temporary JWT with username but no role
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: body
+ *          name: createemp
+ *          description: The employee's info for account you are creating
+ *          schema:
+ *              type: object
+ *              required:   
+ *                  - id
+ *                  - dateTime
+ *                  - role
+ *                  - name
+ *                  - username
+ *                  - email
+ *                  - password
+ *              properties:
+ *                  id:
+ *                      type: string
+ *                  dateTime:
+ *                      type: string
+ *                  role:
+ *                      type: string
+ *                  name:
+ *                      type: string
+ *                  username:
+ *                      type: string
+ *                  email:
+ *                      type: string
+ *                  password:
+ *                      type: string
+ *      responses:
+ *          200:
+ *              description: Will return temp JWT auth token and refresh token when user created
+ */
+
 app.post('/createemp', jsonParser, async (req, res) => {
     // read username and password from request body
     const { input } = req.body;
@@ -187,6 +392,16 @@ app.post('/createemp', jsonParser, async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /token:
+ *  post:
+ *      summary: refreshes JWT
+ *      responses:
+ *          200:
+ *              description: Sucess
+ */
+
 app.post('/token', (req, res) => {
     const { token } = req.body;
 
@@ -211,11 +426,44 @@ app.post('/token', (req, res) => {
     });
 });
 
+/**
+ * @swagger
+ * /number:
+ *  post:
+ *      summary: get next issued number from database collection
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: body
+ *          name: number
+ *          description: Id of collection you want number from
+ *          schema:
+ *              type: object
+ *              required:   
+ *                  - id
+ *              properties:
+ *                  id:
+ *                      type: string
+ *      responses:
+ *          200:
+ *              description: Will return object containing number and equation to gen next number
+ */
+
 app.post('/number', jsonParser, async(req,res)=>{
     let number = await dal.getNumber(req.body.id)
     console.log("number",number)
     res.send(number)
 });
+
+/**
+ * @swagger
+ * /logout:
+ *  post:
+ *      summary: logout and erase all active refresh tokens
+ *      responses:
+ *          200:
+ *              description: Sucess
+ */
 
 app.post('/logout', (req, res) => {
     const { token } = req.body;
